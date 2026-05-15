e4ff54eb9f99041781432f6c357ba9688a1e7ed3
Michael Yeates <mdyeates@amazon.com>
2026-05-15 21:33:35 +0100

    Remove prerender as its making the theme stale

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,8 +3,6 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Server-side render by default. Set to `false` for SPA mode.
   ssr: true,
-  // Pre-render the landing page at build time.
-  prerender: ["/"],
   // Enable middleware.
   future: {
     v8_middleware: true,
